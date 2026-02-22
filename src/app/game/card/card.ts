7c78879a58f97270bed2ea386650b08db0f71ef3
GabrielHdzDaw <gabrielhernandezdaw@gmail.com>
2026-02-22 16:30:39 +0100

    card face layout done

--- src/app/game/card/card.ts
+++ src/app/game/card/card.ts
@@ -1,4 +1,17 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+
+const PIP_POSITIONS: Record<string, [number, number][]> = {
+  'A':  [[2,3]],
+  '2':  [[2,1], [2,5]],
+  '3':  [[2,1], [2,3], [2,5]],
+  '4':  [[1,1], [3,1], [1,5], [3,5]],
+  '5':  [[1,1], [3,1], [2,3], [1,5], [3,5]],
+  '6':  [[1,1], [3,1], [1,3], [3,3], [1,5], [3,5]],
+  '7':  [[1,1], [3,1], [2,2], [1,3], [3,3], [1,5], [3,5]],
+  '8':  [[1,1], [3,1], [2,2], [1,3], [3,3], [2,4], [1,5], [3,5]],
+  '9':  [[1,1], [3,1], [1,2], [3,2], [2,3], [1,4], [3,4], [1,5], [3,5]],
+  '10': [[1,1], [3,1], [2,2], [1,2], [3,2], [1,4], [3,4], [2,4], [1,5], [3,5]],
+};
 
 @Component({
   selector: 'card',
@@ -10,5 +23,7 @@ export class Card {
   value = input<string>();
   suit = input<string>();
 
+  pipPositions = computed(() => PIP_POSITIONS[this.value() ?? ''] ?? []);
   
 }
+
